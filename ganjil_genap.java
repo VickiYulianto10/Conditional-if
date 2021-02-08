9f8ff9e7c8998197ded298f37355e1248ef7a1f0
VickiYulianto10 <77873680+VickiYulianto10@users.noreply.github.com>
2021-02-08 08:58:36 +0700

    Add files via upload

---
+++ ganjil_genap.java
@@ -0,0 +1,13 @@
+public class ganjil_genap {
+     public static void main(String[] args) {
+     int b = 31;
+
+     System.out.println("nilai = " + b);
+
+     if (b % 2 == 1 ) {
+        System.out.println("adalah bilangan ganjil");
+     }else{
+        System.out.println("adalah bilangan genap");
+     }
+ }   
+}
